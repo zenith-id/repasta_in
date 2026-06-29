eea92c36b69f050e33067c010eb6ea2276c113e0
gybran nauval <nauvalgybran@gmail.com>
2026-06-29 22:36:21 +0700

    feat(seo): add detailed googlebot directives to robots meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,7 +104,17 @@ export const metadata: Metadata = {
       },
     ],
   },
-  robots: { index: true, follow: true },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
   other: { "article:modified_time": "2026-06-28" },
 };
 
